6df8a0b2612337f64e539bc8cf6ebbb77cbb0ac3
Br-Fernandes <93941255+Br-Fernandes@users.noreply.github.com>
2024-07-16 22:39:40 -0300

    Update CalculadoraTest.java

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -8,41 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculadoraTest {
 
-    Calculadora calculadora;
-
-    @BeforeEach
-    public void setUp() {
-        calculadora = new Calculadora();
-    }
-
-    @Test
-    public void testarSoma() {
-        assertEquals(6, calculadora.somar(3,3));
-    }
-
-    @Test
-    public void testarSubtracao() {
-        assertEquals(5, calculadora.subtrair(12,7));
-    }
-
-    @Test
-    public void testarMultiplicacao() {
-        assertEquals(20, calculadora.multiplicar(10,2));
-    }
-
-    @Test
-    public void testarDivisao() {
-        assertEquals(2, calculadora.dividir(10,5));
-    }
-
-    @Test
-    public void testarDivisaoPorZero() {
-        assertThrows(IllegalArgumentException.class, () -> calculadora.dividir(5, 0));
-    }
-
-    @AfterEach
-    public void tearDown() {
-        calculadora = null;
-    }
+   
 
 }
